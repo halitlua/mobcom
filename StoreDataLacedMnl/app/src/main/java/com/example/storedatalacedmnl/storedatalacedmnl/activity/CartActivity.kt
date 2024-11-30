feeb9eb7ce977a8eb1ae87cd2aa85236e77f4c5a
Stephen Jed Fetalino <96241556+Landotin@users.noreply.github.com>
2024-11-30 23:15:24 +0800

    Updated Backbutton

--- StoreDataLacedMnl/app/src/main/java/com/example/storedatalacedmnl/storedatalacedmnl/activity/CartActivity.kt
+++ StoreDataLacedMnl/app/src/main/java/com/example/storedatalacedmnl/storedatalacedmnl/activity/CartActivity.kt
@@ -44,7 +44,7 @@ class CartActivity : BaseActivity() {
 
     private fun calculateCart() {
         val percentTax = 0.02
-        val delivery = 10.0
+        val delivery = 0
         tax = Math.round((managmentCart.getTotalFee() * percentTax) * 100) / 100.0
         val total = Math.round((managmentCart.getTotalFee() + tax + delivery) * 100) / 100
         val itemTotal = Math.round(managmentCart.getTotalFee() * 100) / 100
